6c144e03413e2d7b1330fa9de9f30c7be6e266c0
unknown <sijibomiolajubu@gmail.com>
2021-12-28 06:02:39 +0100

    basic UI structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,15 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200"> Hello world </div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className=' text-2xl text-center'>Which Pokemon is rounder</div>
+      <div className='p-2' />
+      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200'/>
+        <div className='p-8'> VS</div>
+        <div className='w-16 h-16 bg-red-200'/>
+      </div>
+    </div>
   )
 }
 
