8397d4fd7590b9353d4cfe8947b3ffe4b4589de7
unknown <sijibomiolajubu@gmail.com>
2021-12-28 13:40:17 +0100

    add prisma and voting end point

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,5 +1,6 @@
 import * as trpc from '@trpc/server';
 import { PokemonClient } from 'pokenode-ts';
+import { prisma } from "@/backend/utils/prisma";
 import { z } from 'zod';
 export const appRouter = trpc.router().query("get-pokemon-by-id",{
   input: z.object({ id: z.number() }),
@@ -8,8 +9,22 @@ export const appRouter = trpc.router().query("get-pokemon-by-id",{
 
     const pokemon = await api.getPokemonById(input.id)
 
-    return { name: pokemon.name, sprites: pokemon.sprites }
+    return { id: pokemon.id, name: pokemon.name, sprites: pokemon.sprites, spriteUrl: pokemon.sprites, }
   }
-})
+}).mutation("cast-vote", {
+  input: z.object({
+    votedFor: z.number(),
+    votedAgainst: z.number(),
+  }),
+  async resolve({ input }) {
+    const voteInDb = await prisma.vote.create({
+      data: {
+        votedAgainstId: input.votedAgainst,
+        votedForId: input.votedFor,
+      },
+    });
+    return { success: true, vote: voteInDb };
+  },
+});
 
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
+import { inferQueryResponse } from './api/trpc/[trpc]'
 
+const btn =
+  "inline-flex mt-2 mb-2 items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 const Home: NextPage = () => {
   const [ids , updateIds] = useState(getOptionsForVote());
@@ -21,24 +24,35 @@ const Home: NextPage = () => {
   if ( firstPokemon.isLoading || secondPokemon.isLoading || !firstPokemon.data?.sprites.front_default
     || !secondPokemon.data?.sprites.front_default ) return null
 
+    const voteForRoundest  = (selectedNumber: number) => {
+
+      updateIds(getOptionsForVote())
+    }
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className=' text-2xl text-center'>Which Pokemon is rounder</div>
       <div className='p-2' />
-      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-64 h-64 flex flex-col'>
+      <div className='border rounded p-12 flex justify-between items-center max-w-2xl'>
+        { !firstPokemon.isLoading  && firstPokemon.data && !secondPokemon.isLoading && (
+        <>
+        <div className='w-64 h-64 flex flex-col items-center'>
           <img src={firstPokemon.data?.sprites.front_default} className='w-full'/>
           <div className='text-xl text-center capitalize mt-[-2rem]'>{firstPokemon.data.name}</div>
+          <button className={btn} onClick={()=> voteForRoundest(first)}>Rounder</button>
         </div>
         <div className='p-8'> VS</div>
-        <div className='w-64 h-64 flex flex-col'>
+        <div className='w-64 h-64 flex flex-col items-center'>
           <img src={secondPokemon.data.sprites.front_default} className='w-full' />
           <div className='text-xl text-center capitalize mt-[-2rem]'>{secondPokemon.data.name}</div>
+          <button className={btn} onClick={()=> voteForRoundest(second)}>Rounder</button>
         </div>
+        </>
+        )}
         <div className='p-2' />
       </div>
     </div>
   )
 }
 
+type pokemonFromServer = inferQueryResponse<"get-pokemon-by-id">
 export default Home
